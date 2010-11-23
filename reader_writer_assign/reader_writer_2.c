84a645cee0f5dfcdb0e76f0741335ca24ed69913
jbotnascimento <jbotnascimento@fbcd9c34-cb60-2131-5c94-29eb91f86444>
2010-11-23 13:46:50 +0000

    solving question 3.1

---
+++ reader_writer_assign/reader_writer_2.c
@@ -0,0 +1,52 @@
+#include "reader_writer_2.h"
+#include "reader_writer_tracing.h"
+#include "stdio.h"
+extern tracing_t t;
+
+pthread_mutex_t mutex;
+
+
+void init(reader_writer_t rw){
+  pthread_mutex_init(&mutex,NULL);
+}
+
+void begin_read(reader_writer_t rw){
+  /* ... */
+  //pthread_mutex_lock( &mutex );
+  tracing_record_event(t, BR_EVENT_ID);
+  /* ... */
+
+}
+
+void end_read(reader_writer_t rw){
+  /* ... */
+  //pthread_mutex_unlock( &mutex );
+  tracing_record_event(t, ER_EVENT_ID);
+
+  /* ... */
+
+
+}
+
+void begin_write(reader_writer_t rw){
+  /* ... */
+  pthread_mutex_lock( &mutex );
+  tracing_record_event(t, BW_EVENT_ID);
+
+  /* ... */
+
+
+
+}
+
+void end_write(reader_writer_t rw){
+  /* ... */
+  pthread_mutex_unlock( &mutex );
+  tracing_record_event(t, EW_EVENT_ID);
+
+  /* ... */
+
+
+
+}
+
