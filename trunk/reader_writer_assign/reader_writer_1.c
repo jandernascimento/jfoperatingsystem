3fd226fae58d88eccec63564d5ca92977f613d3b
jbotnascimento <jbotnascimento@fbcd9c34-cb60-2131-5c94-29eb91f86444>
2010-11-27 19:04:12 +0000

    removing unused lines

--- trunk/reader_writer_assign/reader_writer_1.c
+++ trunk/reader_writer_assign/reader_writer_1.c
@@ -5,50 +5,27 @@ extern tracing_t t;
 
 pthread_mutex_t mutex;
 
-
 void init(reader_writer_t rw){
   pthread_mutex_init(&mutex,NULL);
 }
 
 void begin_read(reader_writer_t rw){
-  /* ... */
   pthread_mutex_lock( &mutex );
   tracing_record_event(t, BR_EVENT_ID);
-
-  /* ... */
-
-
 }
 
 void end_read(reader_writer_t rw){
-  /* ... */
   pthread_mutex_unlock( &mutex );
   tracing_record_event(t, ER_EVENT_ID);
-
-  /* ... */
-
-
 }
 
 void begin_write(reader_writer_t rw){
-  /* ... */
   pthread_mutex_lock( &mutex );
   tracing_record_event(t, BW_EVENT_ID);
-
-  /* ... */
-
-
-
 }
 
 void end_write(reader_writer_t rw){
-  /* ... */
   pthread_mutex_unlock( &mutex );
   tracing_record_event(t, EW_EVENT_ID);
-
-  /* ... */
-
-
-
 }
 
